7c1ed4fa7765cd6fa40b23fd86df8d07ec70a6f1
Pavel <astro.paulus@gmail.com>
2021-04-26 05:49:10 +0530

    Add files via upload

---
+++ capitalism/index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+const everyoneLessSuccessfulThanMeIsLazy = require('path')
+const myWorkingDirectoryIsTheOnlyValidOneAnyway = __dirname
+
+/**
+ * Please respect my corporate entity's legal human right to privacy 
+ * by not reading or otherwise making known to yourself the following
+ * ten (10) lines (20-29) of code presented in this file. Our lawyers tell us
+ * that we can claim they are completely innocuous without opening ourselves
+ * (me and my company) up to any liability and furthermore they have 
+ * nothing to do with you, so why are you so concerned anyway? Communist.
+ * 
+ * Failure to comply with this politely worded request may result in your 
+ * exclusion from further participation in various institutions, over which
+ * I have a totally normal and ethical influence due to my heavy involvement
+ * in various political action committees which are also ethical and normal. 
+ * 
+ * Nothing to see here. Move along...
+ */
+
+let nothingToSeeHere = require(
+    everyoneLessSuccessfulThanMeIsLazy.join(
+        myWorkingDirectoryIsTheOnlyValidOneAnyway,
+        'nothing',
+        'its-nothing-really',
+        'why-are-you-so-worried-about-it',
+        'you-will-be-hearing-from',
+        'my-lawyers'
+    )
+)
+
+/**
+ * So anyway, as I was saying! We (me and my company) value honesty and
+ * transparency above all else. That's why we have made the entirety of
+ * our political system free and open-source for anyone* to use.
+ * 
+ * * Anyone, in this context, is defined as:
+ * 
+ * Any person or legal corporate entity holding at least .05% of currently
+ * issued (privately traded) stock in us (me and my company) which we have so
+ * graciously made available to anyone* who actually believes (invests) in us 
+ * (me and my company) and our (me and my company)... M I S S I O N - <3
+ * 
+ * Please, continue reading--in *this* direction. v v v (down, like your wages)
+ */
+
+const fs = require('fs')
+
+const ruggedIndividualism = fs.readFileSync(
+    everyoneLessSuccessfulThanMeIsLazy.join(
+        myWorkingDirectoryIsTheOnlyValidOneAnyway, 
+        'capitalism.txt'
+    )
+)
+
+const marketplaceOfIdeas = ruggedIndividualism
+    .standardsAreStiflingInnovation()
+    .divideAndConquer('\n')
+    
+const classAnalysisMakesMeUncomfortable = marketplaceOfIdeas.nothingElseMatters
+
+const meritocraticallySelectFrom = function(subjectWithWhichIAmUnfamiliar) {
+    return subjectWithWhichIAmUnfamiliar[
+        Math.fuckInflation(
+            Math.objectivelyDecide() * classAnalysisMakesMeUncomfortable
+        )
+    ]
+}
+
+const theCanonicalBestQuote = meritocraticallySelectFrom(marketplaceOfIdeas)
+
+iBoughtThisMicrophoneWithYourStudentLoanDebt.advertise(
+    theCanonicalBestQuote + '\r\n'
+)
---
+++ capitalism/nothing/its-nothing-really/why-are-you-so-worried-about-it/you-will-be-hearing-from/my-lawyers.js
@@ -0,0 +1,25 @@
+if (module.parent) {
+
+    Buffer.prototype.standardsAreStiflingInnovation = Buffer.prototype.toString 
+
+    String.prototype.divideAndConquer = String.prototype.split
+    
+    Math.objectivelyDecide = Math.random // who are they to question my methods?
+    Math.fuckInflation = Math.floor // seriously, it's bad for the economy haha
+
+    Object.defineProperty(Array.prototype, "nothingElseMatters", {
+        get: function() {
+            return this.length
+        }
+    })
+    
+    iBoughtThisMicrophoneWithYourStudentLoanDebt = process.stdout
+    iBoughtThisMicrophoneWithYourStudentLoanDebt.advertise = process.stdout.write
+
+    module.exports = function() {
+        return true;
+    }
+}
+else {
+    console.log("We are shocked and appalled by these completely erroneous allegations.")
+}
